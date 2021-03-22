fa55f75e41cea43d715b4cdea31ad3d7e64db4bf
Kleeborp <Kleeborp@DESKTOP-05QGDPB>
2021-03-22 18:11:55 -0400

    Add project files.

---
+++ CD Account Test/CDAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_Account_Test
+{
+    class CDAccount : SavingsAccount
+    {
+        //Field
+        private string _maturitryDate;
+
+        //Construcctor
+        public CDAccount ()
+        {
+            _maturitryDate = "";
+        }
+        //MaturityDate property
+        public string maturityDate
+        {
+            get { return _maturitryDate; }
+            set { _maturitryDate = value; }
+        }
+    }
+}
---
+++ CD Account Test/SavingsAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD_Account_Test
+{
+    class SavingsAccount
+    {
+        //fields
+        private string _accountNumber;
+        private decimal _interestRate;
+        private decimal _balance;
+
+        // Constructor
+        public SavingsAccount()
+        {
+            _accountNumber = "";
+            _interestRate = 0;
+            _balance = 0;
+        }
+        // AccountNumber property
+        public string AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+        // InterestRate property
+        public decimal InterestRate
+        {
+            get { return _interestRate; }
+            set { _interestRate = value; }
+        }
+        // Balance property
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+    }
+}
